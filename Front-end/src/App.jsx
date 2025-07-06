2bb33da34914142d4f0a5e78cf188bd83e86a057
UtkristaPokharel <pokharelutkrista@gmail.com>
2025-07-06 19:06:34 +0545

    FAQ added

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -6,16 +6,18 @@ import VehicleTypePage from './components/VehicleTypePage';
 import AuthForm from './pages/Auth.jsx';
 import VehicleDetails from './pages/VehicleDetails.jsx';
 import Profile from './pages/Profile.jsx';
+import FAQPage from './pages/FAQPage.jsx';
 
 export default function App() {
   return (
-     <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<AuthForm/>}/>
+        <Route path="/login" element={<AuthForm />} />
         <Route path="/vehicles/:type" element={<VehicleTypePage />} />
-        <Route path="/vehicle/:type/:id" element={<VehicleDetails/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/vehicle/:type/:id" element={<VehicleDetails />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/faq" element={<FAQPage />} />
       </Routes>
     </Router>
   )
--- Front-end/src/components/Navbar.jsx
+++ Front-end/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { IoMenu, IoClose } from "react-icons/io5";
 import profile from "../../public/whiteprofile.svg"
 
 
+
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     const menuref = useRef(null);
@@ -33,18 +34,19 @@ export default function Navbar() {
                 <div> <img src="/logo.png" alt="EasyWheels Logo" className="h-30" /></div>
                 <div className='flex gap-10 items-center text-white text-lg '>
                     <ul className='hidden xl:flex space-x-9'>
-                        <li><a href="#" className=' hover:decoration-red-600 decoration-3 hover:underline hover:underline-offset-8'>Home</a></li>
+                        <li><a href="/" className=' hover:decoration-red-600 decoration-3 hover:underline hover:underline-offset-8'>Home</a></li>
                         <li><a href="#" className=' hover:decoration-red-600 decoration-3 hover:underline hover:underline-offset-8'>About us</a></li>
                         <li><a href="#" className=' hover:decoration-red-600 decoration-3 hover:underline hover:underline-offset-8'>Vehicles</a></li>
                         <li><a href="#" className=' hover:decoration-red-600 decoration-3 hover:underline hover:underline-offset-8'>Contact us</a></li>
+                        <li><a href="/faq" className=' hover:decoration-red-600 decoration-3 hover:underline hover:underline-offset-8'>FAQ</a></li>
                     </ul>
 
-                    <button className="border-0 px-4 py-2 bg-red-600 text-white text-sm md:text-base rounded-2xl hover:cursor-pointer hover:scale-110 transition-transform duration-200">
+                    <button className="flex border-0 px-4 py-2 bg-red-600 text-white text-sm md:text-base rounded-2xl hover:cursor-pointer hover:scale-110 transition-transform duration-200">
                         Become a host
                     </button>
 
                     <div className='profile-icon  '>
-                        <img src={profile} className='w-9' alt="" />
+                        <a href="/profile"><img src={profile} className='w-9' alt="" /></a>
                     </div>
 
                     <button className='xl:hidden text-white' onClick={handleToggle}>
@@ -55,12 +57,13 @@ export default function Navbar() {
                     <div
                         ref={menuref}
                         className={` xl:hidden text-white absolute top-16 right-0 bg-gray-600 w-30
-          h-30 list-none pl-5 transition-all duration-300 ease-in-out 
+          h-35 list-none pl-5 transition-all duration-300 ease-in-out 
             ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-2 pointer-events-none'}`}>
-                        <li><a href="#" className='text-white hover:text-gray-400'>Home</a></li>
+                        <li><a href="/" className='text-white hover:text-gray-400'>Home</a></li>
                         <li><a href="#" className='text-white hover:text-gray-400'>About us</a></li>
                         <li><a href="#" className='text-white hover:text-gray-400'>Vehicles</a></li>
                         <li><a href="#" className='text-white hover:text-gray-400'>Contact us</a></li>
+                        <li><a href="/faq" className='text-white hover:text-gray-400'>FAQ</a></li>
                     </div>
 
 
