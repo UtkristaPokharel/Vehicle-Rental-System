be380fd3a36341d0c90571104a5d262c1fcb084a
UtkristaPokharel <pokharelutkrista@gmail.com>
2025-07-06 15:37:29 +0545

    localhost change

--- Front-end/src/pages/Auth.jsx
+++ Front-end/src/pages/Auth.jsx
@@ -8,7 +8,7 @@ import axios from "axios";
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://localhost:3001/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
